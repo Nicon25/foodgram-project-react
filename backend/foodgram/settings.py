384959b897348640b7ada352634027c7e9716130
Nikolai Potapov <potapov.nikolai.v@gmail.com>
2023-09-04 22:15:12 +0100

    add apps to settings.py, add djoser

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,6 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'recipes',
+    'users',
+    'djoser'
 ]
 
 MIDDLEWARE = [
