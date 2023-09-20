686e743222d0252fdd959d30445cba5eb202ecff
Nikolai Potapov <potapov.nikolai.v@gmail.com>
2023-09-20 22:22:19 +0100

    add docker settings, actions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR.parent, '.env'))
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-u8r86g51+7^==y*sspxd+m=11s-)-&=gernfw5fdf2t)w)wz%y'
 
 DEBUG = True
 
@@ -61,7 +61,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
-if DEBUG:
+if DEBUG == False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -71,12 +71,18 @@ if DEBUG:
 else:
     DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': env('POSTGRES_DB', default='django'),
+        # 'USER': env('POSTGRES_USER', default='django'),
+        # 'PASSWORD': env('POSTGRES_PASSWORD', default=''),
+        # 'HOST': env('DB_HOST', default=''),
+        # 'PORT': env.int('DB_PORT', 5432)
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB', default='django'),
-        'USER': env('POSTGRES_USER', default='django'),
-        'PASSWORD': env('POSTGRES_PASSWORD', default=''),
-        'HOST': env('DB_HOST', default=''),
-        'PORT': env.int('DB_PORT', 5432)
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
@@ -114,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/backend_static/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
