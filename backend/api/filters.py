db7c966e25405c706a1fa0fc65c77cdab45b668a
Nikolai Potapov <potapov.nikolai.v@gmail.com>
2023-09-26 13:08:17 +0100

    filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -6,10 +6,16 @@ class RecipeFilter(django_filters.FilterSet):
     is_favorited = django_filters.BooleanFilter(
         field_name="is_favorited",
         method="filter_is_favorited",
+        label="is_favorited",
     )
     is_in_shopping_cart = django_filters.BooleanFilter(
         field_name="is_in_shopping_cart",
         method="filter_is_in_shopping_cart",
+        label="is_in_shopping_cart",
+    )
+    tags = django_filters.AllValuesMultipleFilter(
+        field_name='tags__slug',
+        label='tags',
     )
 
     def filter_is_favorited(self, queryset, name, value):
@@ -27,4 +33,6 @@ class RecipeFilter(django_filters.FilterSet):
         fields = {
             "author": ["exact"],
             "tags": ["exact"],
+            'is_favorited': ["exact"],
+            'is_in_shopping_cart': ["exact"],
         }
--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,5 +2,4 @@ from rest_framework import pagination
 
 
 class LimitPagination(pagination.PageNumberPagination):
-    page_size = 6
     page_size_query_param = 'limit'
