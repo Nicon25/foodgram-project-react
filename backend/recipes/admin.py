950d796477cf9edd19f8909dfe9a14903556d099
Nikolai Potapov <potapov.nikolai.v@gmail.com>
2023-09-28 14:48:19 +0100

    reset

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,9 +3,25 @@ from django.contrib import admin
 from .models import (Favorites, Ingredient, IngredientInRecipe, Recipe,
                      ShoppingCart, Tag)
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'cooking_time',
+        'text', 'author', 'favorites_count'
+    )
+    list_filter = ('name', 'author', 'tags')
+
+    def favorites_count(self, object):
+        return object.is_favorited.count()
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+
+
 admin.site.register(Tag)
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(IngredientInRecipe)
 admin.site.register(ShoppingCart)
 admin.site.register(Favorites)
